34f7bff31edc71c77613a3d9f32a0556d5881816
Marcus Robertsson <marcus.asberg.98@gmail.com>
2024-08-29 19:23:20 +0200

    Okay searching is kinda working, but not with spaces :( if it finds nothing it crasches haah

--- main.go
+++ main.go
@@ -36,33 +36,49 @@ type TemplateData struct {
 	VoteAverage float64
 }
 
-type Page struct {
-	Title string
-	Body  []byte
-}
 type App struct {
 	APIKey string
 }
 
 func (app *App) indexHandler(w http.ResponseWriter, r *http.Request) {
-	url := "https://api.themoviedb.org/3/account/21472664/favorite/movies?language=en-US&page=1&sort_by=created_at.asc"
+	searchQuery := r.URL.Query().Get("search")
+	var url string
+
+	if searchQuery != "" {
+		// Use the search movies endpoint
+		url = fmt.Sprintf("https://api.themoviedb.org/3/search/movie?query=%s&language=en-US&page=1&include_adult=false", searchQuery)
+	} else {
+		// Default to fetching favorite movies
+		url = "https://api.themoviedb.org/3/account/21472664/favorite/movies?language=en-US&page=1&sort_by=created_at.asc"
+	}
 
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("accept", "application/json")
 	req.Header.Add("Authorization", "Bearer "+app.APIKey)
 
-	res, _ := http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		http.Error(w, "Failed to fetch movies", http.StatusInternalServerError)
+		return
+	}
 	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		http.Error(w, "Failed to fetch movies", http.StatusInternalServerError)
+		return
+	}
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, "Failed to read response", http.StatusInternalServerError)
+		return
 	}
 
 	var apiResponse APIResponse
 	err = json.Unmarshal(body, &apiResponse)
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, "Failed to parse response", http.StatusInternalServerError)
+		return
 	}
 
 	var data TemplateData
