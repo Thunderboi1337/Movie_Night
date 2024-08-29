160f99bf4000fcf909232eb058afe60c48e9f6ca
Marcus Robertsson <marcus.asberg.98@gmail.com>
2024-08-29 17:02:05 +0200

    Hiding the key for future use, but will have to change it anyways

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"text/template"
+
+	"github.com/joho/godotenv"
 )
 
 type Movie struct {
@@ -38,56 +40,16 @@ type Page struct {
 	Title string
 	Body  []byte
 }
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
+type App struct {
+	APIKey string
 }
 
-func loadPage(title string) *Page {
-	filename := title + ".txt"
-	body, _ := os.ReadFile(filename)
-	return &Page{Title: title, Body: body}
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p := loadPage(title)
-	if p != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
-	p := loadPage(title)
-	if p != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	p.save()
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func (app *App) indexHandler(w http.ResponseWriter, r *http.Request) {
 	url := "https://api.themoviedb.org/3/account/21472664/favorite/movies?language=en-US&page=1&sort_by=created_at.asc"
 
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("accept", "application/json")
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YTcwZWY0YTRiODM0MzYyYmRjNzNkNDc2YmJiYzdmMyIsIm5iZiI6MTcyNDkyMzQ1My4yMDc1NjEsInN1YiI6IjY2ZDAzYWY2Yjg4YzIxOTMyYjY2ZmZhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kXlGmpCLk_v1qttPHp5XFWmi4bQ3PpAI3XaS-9792wc")
+	req.Header.Add("Authorization", "Bearer "+app.APIKey)
 
 	res, _ := http.DefaultClient.Do(req)
 	defer res.Body.Close()
@@ -107,7 +69,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	if len(apiResponse.Results) > 0 {
 		data = TemplateData{
-			Movies: apiResponse.Results[:2], // Limiting to first 10 movies
+			Movies: apiResponse.Results[:2],
 		}
 	} else {
 		fmt.Println("No movies found in the response.")
@@ -122,39 +84,21 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		log.Fatal("API_KEY is not set")
+	}
+
+	app := &App{APIKey: apiKey}
 
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
-
-	url := "https://api.themoviedb.org/3/account/21472664/favorite/movies?language=en-US&page=1&sort_by=created_at.asc"
-
-	req, _ := http.NewRequest("GET", url, nil)
-
-	req.Header.Add("accept", "application/json")
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YTcwZWY0YTRiODM0MzYyYmRjNzNkNDc2YmJiYzdmMyIsIm5iZiI6MTcyNDkyMzQ1My4yMDc1NjEsInN1YiI6IjY2ZDAzYWY2Yjg4YzIxOTMyYjY2ZmZhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kXlGmpCLk_v1qttPHp5XFWmi4bQ3PpAI3XaS-9792wc")
-
-	res, _ := http.DefaultClient.Do(req)
-
-	//var movie Moives
-
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var apiResponse APIResponse
-	err = json.Unmarshal(body, &apiResponse)
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/", app.indexHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
