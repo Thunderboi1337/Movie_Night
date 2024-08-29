fdc2866dd3c63c5270322ea01669d2308fe9e8d1
Marcus Robertsson <marcus.asberg.98@gmail.com>
2024-08-29 15:17:55 +0200

    I can now call the data from the api, will try to add it to the html

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"text/template"
 )
+
+type Movie struct {
+	Title       string  `json:"title"`
+	Overview    string  `json:"overview"`
+	PosterPath  string  `json:"poster_path"`
+	ReleaseDate string  `json:"release_date"`
+	VoteAverage float64 `json:"vote_average"`
+}
+
+type APIResponse struct {
+	Page         int     `json:"page"`
+	Results      []Movie `json:"results"`
+	TotalPages   int     `json:"total_pages"`
+	TotalResults int     `json:"total_results"`
+}
 
 type Page struct {
 	Title string
@@ -68,6 +86,44 @@ func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
 	http.HandleFunc("/save/", saveHandler)
+
+	url := "https://api.themoviedb.org/3/account/21472664/favorite/movies?language=en-US&page=1&sort_by=created_at.asc"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YTcwZWY0YTRiODM0MzYyYmRjNzNkNDc2YmJiYzdmMyIsIm5iZiI6MTcyNDkyMzQ1My4yMDc1NjEsInN1YiI6IjY2ZDAzYWY2Yjg4YzIxOTMyYjY2ZmZhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kXlGmpCLk_v1qttPHp5XFWmi4bQ3PpAI3XaS-9792wc")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	//var movie Moives
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var apiResponse APIResponse
+	err = json.Unmarshal(body, &apiResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Accessing the first movie's title
+	if len(apiResponse.Results) > 0 {
+		firstMovie := apiResponse.Results[0]
+		fmt.Println("Title:", firstMovie.Title)
+		fmt.Println("Overview:", firstMovie.Overview)
+		fmt.Println("Poster Path:", firstMovie.PosterPath)
+		fmt.Println("Release Date:", firstMovie.ReleaseDate)
+		fmt.Println("Vote Average:", firstMovie.VoteAverage)
+	} else {
+		fmt.Println("No movies found in the response.")
+	}
+
+	// fmt.Println(string(body))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
